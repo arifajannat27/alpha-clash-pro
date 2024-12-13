e8c6e1e83d591dfa7f503c64970e1bc7c51f6090
arifajannat27 <arifajannat9527@gmail.com>
2024-12-13 12:40:13 +0600

    generate a random alphabet

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,27 @@
+// function play(){
+//     // step-1: hide the home screen. to hide the screen and the class hidden the the section
+//         const homeSection = document.getElementById('home-screen');
+//         homeSection.classList.add('hidden');
+//         // console.log(homeSection.classList);
+
+//     // step-2: Show the playground
+//     const playGround = document.getElementById('play-ground');
+//     playGround.classList.remove('hidden');
+//     // console.log(playGround.classList);
+// }
+
+function continueGame(){
+    // step-1: generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    console.log('Your Alphabet', alphabet);
+
+    // randomly generated alphabet to the screen (show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+}
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame();
+}
